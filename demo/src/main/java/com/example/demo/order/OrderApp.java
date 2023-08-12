68891e9a2b722ff18e1c6e66e3cc15171f569868
catspie92@gmail.com <catspie92@gmail.com>
2023-08-12 15:10:14 +0900

    order 테스트 페이지

---
+++ demo/src/main/java/com/example/demo/order/OrderApp.java
@@ -0,0 +1,25 @@
+package com.example.demo.order;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+
+/**
+ * order 테스트 페이지 
+ */
+public class OrderApp {
+    public static void main(String[] args){
+
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        //toString 메서드가 실행됨
+        System.out.println("order ="+order);
+    }
+}
